b1de455eeb74895ca61d236dad88fd3469f79d80
PCM <popa.calin.marian47@gmail.com>
2024-09-04 20:02:08 +0300

    refacere etapa 4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //etapa 4.1
 const express = require('express');//import express
+const path = require('path')//setez calea
 
 const app = express()//creez instanta al obiectului(serverului in cazul asta) express
 
@@ -9,8 +10,15 @@ console.log(port);//testez portul
 
 
 //setarea vizualizarii paginii pentru ejs
-app.use(express.static(path.join(__dirname, 'resurse')));
+app.use(express.static(path.join(__dirname, 'resurse')));//pentru a accesa resurse
 app.set('view engine', 'ejs');//aici am avut o problema ca imi aparea doar portalul cu caile, de aceea a trebuit sa configurez ejs aici
+app.set('views', path.join(__dirname, 'views'));//pentru a randa paginile de tip ejs din views
+
+// Rute pentru pagina principală
+app.get(['/','/index'], (req, res) => {
+    res.render('pagini/index', { titlu: 'Pagina principală' });
+});
+
 
 app.listen(port, () => {
     console.log(`Serverul asculta la adresa https:localhost:${port}`);
@@ -38,7 +46,3 @@ afiseazaFolder(); afiseazaCaleCompleta(); afiseazaFolderCurent(); // aici testez
 
 //etapa 4.3
 
-// Rute pentru pagina principală
-app.get(['/','/index','/home'], (req, res) => {
-    res.render('index', { titlu: 'Pagina principală' });
-});
